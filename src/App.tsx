77966a5bfa7ac9e9c899b1f6dc8730a22cf66ed2
unknown <email>
2022-09-10 11:20:12 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { type } from '@testing-library/user-event/dist/type';
+import {ChangeEvent} from 'react';
 import './App.css';
+import {useContagem} from './hooks/contagem';
+import { usePeopleList } from './hooks/peopleList';
+import { useState } from 'react';
+
 
 function App() {
+  const[List,Dispatch] = usePeopleList();
+  const [nameInput,setNameInput] = useState('');
+  const handleAddButton = ()=>{
+    if(nameInput)
+    {
+      Dispatch({type:'ADD',payload:{name:nameInput}});
+      setNameInput('')
+    }
+  }
+  const handleInputChange = (e:ChangeEvent<HTMLInputElement>)=>{
+    setNameInput(e.target.value);
+  }
+  const deletePerson = (id:string)=>
+  {
+    Dispatch({
+      type:'DEL',
+      payload:{id}
+    })
+  }
+  const handleOrderButton = () =>
+  {
+    Dispatch({
+      type:'ORDER'
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5">
+      <input className='border-2' type="text" value={nameInput} onChange={handleInputChange}/>
+      <button onClick={handleAddButton}>adicionar</button>
+      <hr />
+      <button className='p-1' onClick={handleOrderButton}>| Ordenar |</button>
+      <hr />
+      lista de pessoas:
+      {List.map((item,index)=>(
+        <li className='p-1' key={index}>
+          {item.name}
+          <button onClick={()=>deletePerson(item.id)}>. |  deletar |</button>
+        </li>
+      ))}
     </div>
   );
 }
